c47a21f50672aa4493a2a5020b0a4c451f55f740
Patrick72444 <patbran07@gmail.com>
2025-04-19 10:57:58 +0200

    Update ema_bot.py

--- ema_bot.py
+++ ema_bot.py
@@ -116,13 +116,11 @@ while True:
 
     print("⏳ Esperando 5 minutos...")
     time.sleep(300)
-    from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Bot funcionando..."
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
